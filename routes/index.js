228b7992b37165083a133a164f1d66a7f0a75a53
Kizito Akhilome <kizito@akhilo.me>
2020-07-03 23:49:22 +0100

    chore: reorg. directory struct.

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const demo = require('./demo.route');
+const r = Router();
+
+r.use('/demo', demo);
+
+r.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'express vercel boiler plate',
+    data: null
+  });
+});
+
+module.exports = r;
